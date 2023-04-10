ff2e643b0e99025865e813a2923ab2b47235bb91
CorbinSpence <corbinspence16@gmail.com>
2023-04-10 00:58:51 -0400

    Added id data and delete function

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ server.get('/api/notes', (req, res)=>{
 })
 
 server.post('/api/notes', (req, res)=>{
-    console.log(req.body)
     const title = req.body["title"]
     const text = req.body["text"]
+    const id = Math.floor(Math.random()*1000000)
     if(title && text){
         fs.readFile('./db/db.json', 'utf8', (err, data)=>{
             let newData = JSON.parse(data)
-            newData.push({"title": title, "text": text})
+            newData.push({"id":id, "title": title, "text": text})
             fs.writeFile('./db/db.json', JSON.stringify(newData), (err)=>err? console.log('Failed to write to db.json.'): console.log('Write successful.'))
             res.status(201).json(newData)
         })
@@ -31,6 +31,22 @@ server.post('/api/notes', (req, res)=>{
         res.status(500).json('Error in finding new db items.')
     }
     
+})
+
+server.delete('/api/notes/:id', (req, res)=>{
+    const id = parseInt(req.params.id)
+    if(id){
+        fs.readFile('./db/db.json', 'utf8', (err, data)=>{
+            let currentData = JSON.parse(data)
+            let newData = currentData.filter((val)=>val["id"]!==id)
+            console.log(id)
+            console.log(newData)
+            fs.writeFile('./db/db.json', JSON.stringify(newData), (err)=>err? console.log('Failed to write to db.json.'): console.log('Removal successful.'))
+            res.status(201).json("Delete succeded")
+        })
+    }else{
+        res.status(500).json('Delete failed')
+    }
 })
 
 server.get('*', (req, res)=>{
