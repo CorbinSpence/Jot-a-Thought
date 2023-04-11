50e983be7539788d7cc925fae1eed2896cb2acc6
CorbinSpence <corbinspence16@gmail.com>
2023-04-11 13:31:02 -0400

    attempt to fix server #1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const fs = require('fs')
 const server =  express()
-const port = 3001
+const port = process.env.PORT || 3001
 
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }));
